2be523508409cc30e1e3c33e43afc1595eacbd04
digallnight <xiaoyong911120@126.com>
2018-08-22 00:16:13 +0800

    常量

---
+++ lqp-common-base/src/main/java/com/lqp/common/base/constant/LqpConstants.java
@@ -0,0 +1,16 @@
+package com.lqp.common.base.constant;
+
+/**
+ * @Auther: xxiao
+ * @Date: 2018/8/21 23:53
+ * @Description:
+ */
+public class LqpConstants {
+    public static final String SUNDAY = "SUNDAY";
+    public static final String MONDAY = "MONDAY";
+    public static final String TUESDAY = "TUESDAY";
+    public static final String WEDNESDAY = "WEDNESDAY";
+    public static final String THURSDAY = "THURSDAY";
+    public static final String FRIDAY = "FRIDAY";
+    public static final String SATURDAY = "SATURDAY";
+}
