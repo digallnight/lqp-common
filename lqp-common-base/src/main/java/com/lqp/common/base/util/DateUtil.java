c2c0b379722d11efcf9dd7261001b237b104bb66
digallnight <xiaoyong911120@126.com>
2018-08-21 23:13:58 +0800

    今天不想继续了，想阿寻了

--- lqp-common-base/src/main/java/com/lqp/common/base/util/DateUtil.java
+++ lqp-common-base/src/main/java/com/lqp/common/base/util/DateUtil.java
@@ -5,11 +5,20 @@ import java.util.Date;
 
 public class DateUtil {
 
+    /**
+     * 获取当前时间戳：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
     public static String now() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return df.format(new Date());
     }
 
+    /**
+     * 获取当前时间戳
+     * @param formatStr
+     * @return
+     */
     public static String now(String formatStr) {
         SimpleDateFormat df = new SimpleDateFormat(formatStr);
         return df.format(new Date());
--- lqp-common-base/src/main/java/com/lqp/common/base/util/EmailUtil.java
+++ lqp-common-base/src/main/java/com/lqp/common/base/util/EmailUtil.java
@@ -1,4 +1,20 @@
 package com.lqp.common.base.util;
 
+import com.lqp.common.base.dict.EnumRegx;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailUtil {
+    /**
+     * 判断是否符合邮箱格式
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isEmail(String str) {
+        Pattern pattern = Pattern.compile(EnumRegx.CHECK_EMAIL.getCode());
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
 }
